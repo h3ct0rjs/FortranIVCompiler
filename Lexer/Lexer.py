b468b01a7fd5242c26e5a761f17e6fbf0710b852
Hector F. Jimenez <hfjimenez@utp.edu.co>
2017-08-13 18:45:08 -0500

    Added Sample Fortran Code

--- Lexer/Lexer.py
+++ Lexer/Lexer.py
@@ -9,15 +9,13 @@ class FortranLexer(Lexer):
                 'DATA', 'DIMMENSION', 'DO','DOUBLE PRECCISION',
                 'END','EQ',
                 'FALSE', 'FORMAT', 'FUNCTION',
-                'GO',
+                'GO TO',
                 'IF','INTEGER',
-                'NE',
                 'PROGRAM',
                 'READ','REAL', 'RETURN', 
                 'SUBROUTINE', 
-                'THEN', 'TO', 'TRUE',
+                'THEN', 'TRUE',
                 }
-
 
     logical = { 'MAYOR', 'MAYOROIGUAL',
                 'MENOR', 'MENOROIGUAL',
@@ -25,11 +23,9 @@ class FortranLexer(Lexer):
                 'NOT', 'AND', 'OR'
                 }
 
-
     # Set of token names.   This is always required
     tokens = {
         'ID',
-        'NUMBER',
         'PLUS',
         'MINUS',
         'TIMES',
@@ -37,19 +33,21 @@ class FortranLexer(Lexer):
         'ASSIGN',
         'LPAREN',
         'RPAREN',
-        *reserved,
+        *reserved,          #Extend the actual tokens
         *logical
         }
-
 
     # String containing ignored characters between tokens
     ignore = ' \t'
     # Set of valid characters
     literals = { '+', '-', '*', '/', '=', '(', ')', '.', ','}
 
-    # Regular expression rules for tokens
+    # Regular expression rules for tokens.
+    # The idea is to match all the coincidences based on gordon-cs.
     ID      = r'[a-zA-Z_][a-zA-Z0-9_]*'
-    NUMBER  = r'\d+'
+    REAL = r'[+-]?([\d]+\.[\d]*|[\d]*\.[0-9]+)?([eE][+-]?[\d]+)?'
+    INTEGER  = r'[+-]?[\d]+'
+    CMPLX = r'CMPLX\([+-]?([+-]?[\d]*([eE]?[\d]*)|[+-]?[\d]*.[\d]+)\,[+-]?([+-]?[\d]*([eE]?[\d]*)|[+-]?[\d]*.[\d]+)\xxx)'
     PLUS    = r'\+'
     MINUS   = r'-'
     TIMES   = r'\*'
@@ -57,6 +55,23 @@ class FortranLexer(Lexer):
     ASSIGN  = r'='
     LPAREN  = r'\('
     RPAREN  = r'\)'
+ 
+    #Triggered action 
+    #if the action is triggered you won't need the above expression, or I guess.
+    def ID(self, t):
+    if t.value in reserved:
+         t.type = t.value.upper()
+    return t
+
+    @_(r'[+-]?([\d]+\.[\d]*|[\d]*\.[0-9]+)?([eE][+-]?[\d]+)?')
+    def REAL(self, t):
+    t.value = float(t.value)   # Convert to a numeric value
+    return t
+
+    @_(r'\d+')
+    def INTEGER(self, t):
+    t.value = int(t.value)   # Convert to a numeric value
+    return t
 
 if __name__ == '__main__':
     data = 'x = 3 + 42 * (s - t)'
