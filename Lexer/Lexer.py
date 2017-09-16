6f8f7a21762513a73df2b166ca88f66c83e313e7
Hector F. Jimenez <hfjimenez@utp.edu.co>
2017-09-16 15:55:06 -0500

    Update Lexer to ignore comments

--- Lexer/Lexer.py
+++ Lexer/Lexer.py
@@ -4,9 +4,9 @@
 from sly import Lexer
 import sys
 from decimal import Decimal as dp
-
 # We inherit from Lexer
 # Reserved Keywords based on
+
 class FortranLexer(Lexer):
     '''
     Lexical Analyzer for our miniFortranIV(Formula Translation 1130/1800) 
@@ -33,33 +33,11 @@ class FortranLexer(Lexer):
     IF :
         IF expr, nl1,nl2,nl3
     '''
-    reserved_words = {'CALL', 'COMMON', 'CONTINUE',           #'CALL_LINK',
-                'DATA', 'DIMMENSION', 'DO', 'DP',
-                'END', 'ELSE', 'EQ', 'EQUIVALENCE',
-                'FALSE', 'FIND', 'FORMAT', 'FUNCTION',
-                'GOTO',
-                'IF', 'INTEGER',
-                'PROGRAM', 'PAUSE',
-                'READ', 'REAL', 'RETURN', 'REWIND',
-                'SUBROUTINE','STOP',
-                'THEN', 'TRUE',
-                'LOGICVALUE', 'LOGICOPERATOR', 'COMPARATOR'
-                }
-
-    others = {'EQ',
-              'GT',
-              'LE',
-              'LT',
-              'GE',
-              'NE'
-              }
-
-    logicaloperator = {'NOT',
-                       'AND',
-                       'OR'
-                       }
-
-    # Set of token names.   This is always required
+    reserved_words = {'CALL', 'CONTINUE', 'DATA', 'DIMMENSION', 'DO', 'DP', 'END', 'ELSE', 'FALSE', 'FIND', 'FORMAT',
+                      'FUNCTION', 'GOTO', 'IF', 'INTEGER', 'PROGRAM', 'PAUSE', 'READ', 'REAL', 'RETURN', 'SUBROUTINE', 'STOP', 'THEN', 'TRUE'}
+    others = {'EQ', 'GT', 'LE', 'LT', 'GE', 'NE'}
+    logicaloperator = {'NOT', 'AND', 'OR'}
+    # Set of token names.
     tokens = {
         'ID',
         'PLUS',
@@ -74,36 +52,29 @@ class FortranLexer(Lexer):
         *logicaloperator,
         *others,
     }
-
+    # Lines star1ting with C or c will be
     # String containing ignored characters between tokens
     ignore = ' \t\r'
-    # Lines star1ting with C or c will be
-    ignore_comment = r'^[cC]{1}\s[a-zA-Z\d ]*'
     # Set of valid characters
     literals = {'+', '-', '*', '/', '=', '(', ')', '.', ','}
-
     # Regular expression rules for tokens.
     # The idea is to match all the coincidences based on 1130/1800 ibm manual.
-    ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
-    REAL = r'[-+]?(([\d]+\.[\d]*|[\d]*\.[\d]+)([eE][-+]?[\d]+)?|[\d]+[eE][-+]?[\d]+)'
-    INTEGER = r'[+-]?[\d]+'
-    #HEXA = r'[zZ][0-9a-fA-F]+'
-    #CMPLX = r'CMPLX\([-+]?(\d|[\d](([\d]*\.?)|([\d]*\.[\d]*))([dD][-+]?[\d]{1,2})|(([\d]+\.[\d]*|[\d]*\.[\d]+)([eE][-+]?[\d]+)?|[\d]+[eE][-+]?[\d]+))\,[-+]?(\d|[\d](([\d]*\.?)|([\d]*\.[\d]*))([dD][-+]?[\d]{1,2})|(([\d]+\.[\d]*|[\d]*\.[\d]+)([eE][-+]?[\d]+)?|[\d]+[eE][-+]?[\d]+))\)'
+    #HEXA = r'(0[xX])?[0-9a-fA-F]+'
+    # Ignores
+    #ignore_comment = r'^C.*'
     PLUS = r'\+'
     MINUS = r'-'
-    TIMES = r'\*'
     EXPONENT = r'\*\*'
+    TIMES = r'\*'
     DIVIDE = r'/'
     ASSIGN = r'='
     LPAREN = r'\('
     RPAREN = r'\)'
-
     TRUE = r'\.TRUE\.',
     FALSE = r'\.FALSE\.',
-
-    NOT=r'\.NOT\.'
-    AND=r'\.AND\.'  
-    OR=r'\.OR\.'
+    NOT = r'\.NOT\.'
+    AND = r'\.AND\.'
+    OR = r'\.OR\.'
     EQ = r'\.EQ\.'
     GT = r'\.GT\.'
     LE = r'\.LE\.'
@@ -111,17 +82,24 @@ class FortranLexer(Lexer):
     GE = r'\.GE\.'
     NE = r'\.NE\.'
     DP = r'DOUBLE\sPRECISION'
-
     # Triggered action
     # if the action is triggered you won't need the above expression, or I
     # guess.
-    @_(r'^[cC]{1}\s[a-zA-Z\d ]*')
-    def t_comment(self, t):
+    # Ignore sNew Lines
+    @_(r'CALL')
+    def CALL(self, t):
+        if t.value in self.reserved_words:
+            t.type = t.value.upper()
+        return t 
+    
+    @_(r'(\n[Cc] *.*)| (^[Cc] *.*)')
+    #@_(r"[cC] *.*")
+    def ignore_comment(self, p):
         pass
 
-    def t_newline(self, t):
-        r'\n+'
-        t.lexer.lineno += len(t.value)
+    @_(r"^C[^a-zA-Z0-9_=]{1}.*")
+    def t_comment(self, p):
+        pass
 
     @_(r'[a-zA-Z_][a-zA-Z0-9_]*')
     def ID(self, t):
@@ -129,7 +107,7 @@ class FortranLexer(Lexer):
             t.type = t.value.upper()
         return t
 
-    @_(r'[-+]?(([\d]+\.[\d]*|[\d]*\.[\d]+)([eE][-+]?[\d]+)?|[\d]+[eE][-+]?[\d]+)')
+    @_(r'[-+]?([\d]+\.[\d]*|[\d]*\.[\d]+)([eE][-+]?[\d]+)?|[\d]+[eE][-+]?[\d]+')
     def REAL(self, t):
         t.value = float(t.value)   # Convert to a numeric value
         return t
@@ -139,24 +117,22 @@ class FortranLexer(Lexer):
         t.value = int(t.value)   # Convert to a numeric value
         return t
 
-    '''
-    @_(r'CMPLX\([-+]?(\d|[\d](([\d]*\.?)|([\d]*\.[\d]*))([dD][-+]?[\d]{1,2})|(([\d]+\.[\d]*|[\d]*\.[\d]+)([eE][-+]?[\d]+)?|[\d]+[eE][-+]?[\d]+))\,[-+]?(\d|[\d](([\d]*\.?)|([\d]*\.[\d]*))([dD][-+]?[\d]{1,2})|(([\d]+\.[\d]*|[\d]*\.[\d]+)([eE][-+]?[\d]+)?|[\d]+[eE][-+]?[\d]+))\)')
-    def CMPLX(self, t):
-        t.value = complex(t.value[0], t.value[1]j)
-        return t
-        '''
-
-    @_(r"DOUBLE PRECISION")
+    # Sets Dp as double precission instance
+    @_(r"DOUBLE\sPRECISION")
     def DP(self, t):
         t.value = dp(t.value)
         return t
+
     # Line number tracking
     @_(r'\n+')
     def ignore_newline(self, t):
         self.lineno += t.value.count('\n')
+    
 
+
+    # Triggers the error
     def error(self, value):
-        print('Line %d: Bad character %r' % (self.lineno, value[0]))
+        print('Line {}: Bad character {}'.format(self.lineno, value[0]))
         self.index += 1
 
     def test(self, data):
@@ -164,12 +140,12 @@ class FortranLexer(Lexer):
         Unitary test
         """
         for tok in lexer.tokenize(data):
-            print('type={}, value={}'.format(tok.type, tok.value))
+            print('type={} value={}'.format(tok.type, tok.value))
         print("DONE")
 
-
 if __name__ == '__main__':
-    sys.argv.append('datasets/integers.fiv')    #Change this line for your dataset
+    # Change this line for your dataset
+    sys.argv.append('datasets/test1')
     if len(sys.argv) != 2:
         sys.stderr.write("Usage: %s filename\n" % sys.argv[0])
         raise SystemExit(1)
