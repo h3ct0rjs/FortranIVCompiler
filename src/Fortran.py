6c360f5c915bda8084d33834cc04fa6bcd4dbfe2
Hector F. Jimenez <hfjimenez@utp.edu.co>
2017-09-24 15:13:23 -0500

    Update src/Fortran.py:rocket:

--- src/Fortran.py
+++ src/Fortran.py
@@ -1,24 +1,41 @@
 from lexer import FortranLexer
-from  parser import FortranParser
+from parser import FortranParser
 import sys
 
+
 def main():
-    sys.argv.append('../Lexer/datasets/test1')
+    sys.argv.append('../datasets/fact.fiv')
     if len(sys.argv) != 2:
-        sys.stderr.write("Usage: %s filename\n" % sys.argv[0])
+        sys.stderr.write('usage: {} filename\n'.format(sys.argv[0]))
         raise SystemExit(1)
 
     lexer = FortranLexer()
     parser = FortranParser()
-    text = open(sys.argv[1]).read()
-    lexer.test(test)
+    data = open(sys.argv[1]).read()
+    print('{}'.format(data))
+    rtok = []
+    for tok in lexer.tokenize(data):
+            print('{}'.format(tok))
+            rtok.append(tok)
+    f=open('../debug/Tokens.out','w')
+    for i  in rtok:
+        f.write(str(i)+'\n')
+    f.close()
+    del rtok
+    print("Tokenization Complete and saved in ../debug/Tokens.out")
+    # print(parser.error)
+    debug = 0
+    p = parser.parse(lexer.tokenize(data))
+    print("Parser Done, We need to add a rule for $\n")
+    print(p)
 
-    while True:
+
+    '''while True:
         try:
             result = parser.parse(lexer.tokenize(text))
             print(result)
         except EOFError:
-            break    
+            break    '''
             
 if __name__ == '__main__':
     main()
